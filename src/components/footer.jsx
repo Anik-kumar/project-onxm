2fef3f364d36d33b4de3d2c6bc28947391af6196
Anik-kumar <Anik-kumar@users.noreply.github.com>
2020-12-12 12:34:06 +0600

    added basic navbar

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h5>This is footer</h5>
+    );
+  }
+}
+ 
+export default Footer;
